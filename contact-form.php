7aa11f0133a7f2691670ef4f7b2f79275371f6bf
alliebourden <127435154+alliebourden@users.noreply.github.com>
2023-08-14 11:43:12 -0230

    more styled contact-form, updated php

--- contact-form.php
+++ contact-form.php
@@ -3,6 +3,15 @@
 if (isset($_POST['submit'])) {
     $fname = $_POST['fname'];
     $lname = $_POST['lname'];
+    $mail = $_POST['mail'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
 }
+
+$mailTo = "alexandreabourden@gmail.com";
+$headers = "From: ".$mailFrom;
+$txt = "You have received an email from ".$name.".\n\n".$message;
+
+
+mail($mailTo, $subject, $txt, $headers);
+header("Location: index.php?mailsend")
