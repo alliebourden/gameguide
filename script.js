04ad2dfdca4d2c643d721f775569323d00185a26
alliebourden <127435154+alliebourden@users.noreply.github.com>
2023-08-21 18:37:48 -0230

    cleaned up fetch

--- script.js
+++ script.js
@@ -6,13 +6,17 @@ fetch('https://api.boardgameatlas.com/api/search?order_by=rank&limit=4&client_id
             console.log('Not Successful')
         }
     })
-    .then(data => console.log(data))
-    .catch(error => console.log('ERROR'))
-
 
 fetch('https://api.boardgameatlas.com/api/search?order_by=rank&limit=4&client_id=JHVAsnwvSz')
     .then(res =>  {
         return res.json();
         })
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
+
+fetch('https://api.boardgameatlas.com/api/search?order_by=rank&limit=4&client_id=JHVAsnwvSz')
+    .then(res => {
+        return res.json();
+    })
     .then(data => console.log(data))
     .catch(error => console.log('ERROR'))
