db73b51ab80ea809d6c68ac8c2c40eb81d7bf399
alliebourden <127435154+alliebourden@users.noreply.github.com>
2023-08-21 17:24:31 -0230

    updated fetch function w order and rank

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-fetch('https://api.boardgameatlas.com/api/search?order_by=rank&ascending=false&client_id=JHVAsnwvSz')
-    .then(res => res.json())
+fetch('https://api.boardgameatlas.com/api/search?order_by=rank&limit=4&client_id=JHVAsnwvSz')
+    .then(res =>  {
+        if (res.ok) {
+            console.log('Success')
+        } else {
+            console.log('Not Successful')
+        }
+    })
     .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
+
+
+fetch('https://api.boardgameatlas.com/api/search?order_by=rank&limit=4&client_id=JHVAsnwvSz')
+    .then(res =>  {
+        return res.json();
+        })
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
