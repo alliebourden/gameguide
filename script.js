4211e86a6a5943ed9550c0406c2276de51c4a5a4
alliebourden <127435154+alliebourden@users.noreply.github.com>
2023-08-22 12:00:07 -0230

    add id to gg tested diff fetch, add params for url

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-fetch('https://api.boardgameatlas.com/api/search?order_by=rank&limit=4&client_id=JHVAsnwvSz')
+let url = "https://api.boardgameatlas.com/api/search?"
+const client_id = "JHVAsnwvSz"
+let players = document.getElementById("players").value
+let playtime = document.getElementById("playtime").value
+
+fetch('https://api.boardgameatlas.com/api/search?min_players=${players}&max_playtime=${playtime}&order_by=rank&limit=6&client_id=JHVAsnwvSz')
     .then(res =>  {
-        if (res.ok) {
-            console.log('Success')
-        } else {
-            console.log('Not Successful')
-        }
-    })
+        return res.json();
+        })
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
+
+fetch('https://api.boardgameatlas.com/api/search?min_players=${players}&order_by=rank&limit=4&client_id=JHVAsnwvSz')
+    .then(res =>  {
+        return res.json();
+        })
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
+
 
 fetch('https://api.boardgameatlas.com/api/search?order_by=rank&limit=4&client_id=JHVAsnwvSz')
     .then(res =>  {
@@ -20,3 +31,13 @@ fetch('https://api.boardgameatlas.com/api/search?order_by=rank&limit=4&client_id
     })
     .then(data => console.log(data))
     .catch(error => console.log('ERROR'))
+
+
+    fetch('https://api.boardgameatlas.com/api/search?order_by=rank&limit=4&client_id=JHVAsnwvSz')
+    .then(res =>  {
+        if (res.ok) {
+            console.log('Success')
+        } else {
+            console.log('Not Successful')
+        }
+    })
