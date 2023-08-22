0f6d6c5e6750957d54097ba820af2b5854a2bac8
alliebourden <127435154+alliebourden@users.noreply.github.com>
2023-08-22 15:06:21 -0230

    updated jc, added text input on gameguide page

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let url = "https://api.boardgameatlas.com/api/search?"
 const client_id = "JHVAsnwvSz"
 let players = document.getElementById("players").value
 let playtime = document.getElementById("playtime").value
+let categories = document.getElementById("categories").value
 
 fetch('https://api.boardgameatlas.com/api/search?min_players=${players}&max_playtime=${playtime}&order_by=rank&limit=6&client_id=JHVAsnwvSz')
     .then(res =>  {
@@ -33,11 +34,18 @@ fetch('https://api.boardgameatlas.com/api/search?order_by=rank&limit=4&client_id
     .catch(error => console.log('ERROR'))
 
 
-    fetch('https://api.boardgameatlas.com/api/search?order_by=rank&limit=4&client_id=JHVAsnwvSz')
+fetch('https://api.boardgameatlas.com/api/search?order_by=rank&limit=4&client_id=JHVAsnwvSz')
     .then(res =>  {
         if (res.ok) {
             console.log('Success')
         } else {
             console.log('Not Successful')
         }
+    })    
+
+fetch('https://api.boardgameatlas.com/api/game/categories?pretty=true&client_id=JLBr5npPhV')
+    .then(res => {
+        return res.json();
     })
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
