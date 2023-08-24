fca658277e0c78cc66f9ee139745c4d265163927
alliebourden <127435154+alliebourden@users.noreply.github.com>
2023-08-24 14:55:49 -0230

    added async for hot games

--- script.js
+++ script.js
@@ -1,52 +1,8 @@
-const url = "https://bgg-json.azurewebsites.net/"
+const url = "https://bgg-json.azurewebsites.net";
 
-let players = document.getElementById("players").value
-let playtime = document.getElementById("playtime").value
-let categories = document.getElementById("categories").value
-let mechanics = document.getElementById("mechanics").value
-
-fetch('https://api.boardgameatlas.com/api/search?min_players=${players}&max_playtime=${playtime}&order_by=rank&limit=6&client_id=JHVAsnwvSz')
-    .then(res =>  {
-        return res.json();
-        })
-    .then(data => console.log(data))
-    .catch(error => console.log('ERROR'))
-
-fetch('https://api.boardgameatlas.com/api/search?min_players=${players}&order_by=rank&limit=4&client_id=JHVAsnwvSz')
-    .then(res =>  {
-        return res.json();
-        })
-    .then(data => console.log(data))
-    .catch(error => console.log('ERROR'))
-
-
-fetch('https://api.boardgameatlas.com/api/search?order_by=rank&limit=4&client_id=JHVAsnwvSz')
-    .then(res =>  {
-        return res.json();
-        })
-    .then(data => console.log(data))
-    .catch(error => console.log('ERROR'))
-
-fetch('https://api.boardgameatlas.com/api/search?order_by=rank&limit=4&client_id=JHVAsnwvSz')
-    .then(res => {
-        return res.json();
-    })
-    .then(data => console.log(data))
-    .catch(error => console.log('ERROR'))
-
-
-fetch('https://api.boardgameatlas.com/api/search?order_by=rank&limit=4&client_id=JHVAsnwvSz')
-    .then(res =>  {
-        if (res.ok) {
-            console.log('Success')
-        } else {
-            console.log('Not Successful')
-        }
-    })    
-
-fetch('https://api.boardgameatlas.com/api/game/categories?pretty=true&client_id=JLBr5npPhV')
-    .then(res => {
-        return res.json();
-    })
-    .then(data => console.log(data))
-    .catch(error => console.log('ERROR'))
+async function getHotGames() {
+    const res = await fetch(`${url}/hot`);
+    console.log(res);
+    const hotgames = await res.json();
+    console.log(hotgames);
+}
