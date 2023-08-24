3c72730077c0a7c561cf797480c0f62c418e9c78
alliebourden <127435154+alliebourden@users.noreply.github.com>
2023-08-24 16:36:46 -0230

    added try and catch err to getHotGame

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const url = "https://bgg-json.azurewebsites.net";
 
 async function getHotGames() {
+    try {
     const res = await fetch(`${url}/hot`);
     console.log(res);
     const hotgames = await res.json();
     console.log(hotgames);
-}
+} catch (err) {
+    console.log(err)
+}}
