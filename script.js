db6f985d6e1e3501cca88799de6662a0a8444451
alliebourden <127435154+alliebourden@users.noreply.github.com>
2023-09-04 14:46:22 -0230

    new function forEach obj, set var

--- script.js
+++ script.js
@@ -9,6 +9,62 @@ hamburgerEl.addEventListener('click', () => {
 
 const url = 'https://bgg-json.azurewebsites.net';
 
+var hotGames = [];
+
+fetch(`${url}/hot`)
+    .then(res => res.json())
+    .then(json => json.forEach(obj =>
+        { let hotGame = {};
+        hotGame.name = obj.name;
+        hotGame.year = obj.yearPublished;
+        hotGame.url = obj.thumbnail;
+        hotGames.push(hotGame);
+        // console.log(hotGames)
+        // console.log(hotGame)
+        }));
+
+
+
+// const getHotGames = async () => {
+//     try {
+//         const res = await fetch(`${url}/hot`);
+//         const hotgames = await res.json();
+//         return hotgames
+//     } catch (err) {
+//         console.log(err)
+//     }
+// }
+    
+
+// function getHotGames() {
+//         fetch('https://bgg-json.azurewebsites.net/hot')
+//         .then(res => {
+//             if (!res.ok) {
+//                 throw Error("ERROR");
+//             }
+//             return res.json();
+//         })
+//         .then(data => {
+//             console.log(data);
+//             const html = data.map(game => {
+//                 return `
+//                 <div class="game">
+//                     <h4>${game.name}</h4>
+//                     <img src="${game.thumbnail}"></img>
+//                     <p>${game.yearPublished}</p>
+//                 </div>
+//                 `;
+//             }).join('');
+//             document
+//                 .querySelector('#hotgame')
+//                 .insertAdjacentHTML("afterbegin", html);
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         });
+//     }
+
+
 // const getHotGames = async () => {
 //     try {
 //         const res = await fetch(`${url}/hot`);
@@ -43,29 +99,40 @@ const url = 'https://bgg-json.azurewebsites.net';
 //     })
 //     } )
 
+// const hotgames = getHotGames();
 
-async function getHotGames() {
-    try {
-    const res = await fetch('https://bgg-json.azurewebsites.net/hot');
-    data = await res.json();
-    const html = data.map(game => {
-        return `
-        <div class="hotgame">
-            <img src="${game.thumbnail}"></img>
-            <h3>${game.name}</h3>
-            <p>${game.yearPublished}</p>
-        </div>
-        `;
-        }).join('');
-        console.log(html);
-        document
-            .querySelector('#hotgame')
-            .insertAdjacentHTML("afterbegin", html);
-    } catch (err) {
-        console.log(err)
-        }
-}
+// async function getHotGames() {
+//     try {
+//     const res = await fetch('https://bgg-json.azurewebsites.net/hot');
+//     data = await res.json();
+//     console.log(data);
+//     return data;
+//     } catch (err) {
+//         console.log(err)
+//         }
+// }
 
+
+
+// async function getHotGames() {
+//     try {
+//     const res = await fetch('https://bgg-json.azurewebsites.net/hot');
+//     data = await res.json();
+//     const html = data.map(game => {
+//         return `
+//             <img src="${game.thumbnail}"></img>
+//             <h3>${game.name}</h3>
+//             <p>${game.yearPublished}</p>
+//         `;
+//         }).join('');
+//         console.log(html);
+//         document
+//             .querySelector('#hotgame')
+//             .insertAdjacentHTML("afterbegin", html);
+//     } catch (err) {
+//         console.log(err)
+//         }
+// }
 
 
 // function getHotGames() {
