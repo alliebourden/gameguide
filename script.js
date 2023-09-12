e71a180433eacffbeaf5a3e54ff316e0dbac1e35
alliebourden <127435154+alliebourden@users.noreply.github.com>
2023-09-12 00:29:36 -0230

    added prevPage event listener

--- script.js
+++ script.js
@@ -58,6 +58,14 @@ document.getElementById('sortByName').addEventListener('click', () => {
         displayGames(game);
 })
 
+document.getElementById('prevPage').addEventListener('click', () => {
+        const totalPages = Math.ceil(games.lenth / resultsPerPage);
+        if (currentPage < totalPages) {
+                currentPage++;
+                displayGames(currentPage);
+        }
+})
+
 // async function getHotGames() {
 //         const res = await fetch(`${url}/hot`);
 //         game = await res.json();
