9ccad6ad7117316cb72a506e75b9278bf93e9281
alliebourden <127435154+alliebourden@users.noreply.github.com>
2023-10-03 19:26:02 -0230

    hamburger menu disappears on scroll or click

--- script.js
+++ script.js
@@ -3,9 +3,36 @@ const navEl = document.querySelector('.nav');
 const hamburgerEl = document.querySelector('.hamburger');
 
 hamburgerEl.addEventListener('click', () => {
-	navEl.classList.toggle('nav--open');
-	hamburgerEl.classList.toggle('hamburger--open');
-})
+    toggleMenu();
+});
+
+
+document.addEventListener('click', (event) => {
+    if (!isInsideMenu(event.target)) {
+        closeMenu();
+    }
+});
+
+
+window.addEventListener('scroll', () => {
+    closeMenu();
+});
+
+function isInsideMenu(element) {
+    return navEl.contains(element) || hamburgerEl.contains(element);
+}
+
+function closeMenu() {
+    if (navEl.classList.contains('nav--open')) {
+        navEl.classList.remove('nav--open');
+        hamburgerEl.classList.remove('hamburger--open');
+    }
+}
+
+function toggleMenu() {
+    navEl.classList.toggle('nav--open');
+    hamburgerEl.classList.toggle('hamburger--open');
+}
 
 // API url
 const url = 'https://bgg-json.azurewebsites.net';
